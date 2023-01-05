deb5c3d735cae5fc9680bb21829d88e7702e1c41
zhangguo <zhangguo@ulearning.cn>
2023-01-05 09:59:05 +0800

    ✨ feat: 添加element-ui 1. 左侧导航和页面框架基本样式 2. eslint和prettier

--- README.md
+++ README.md
@@ -22,3 +22,8 @@ pnpm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+## 格式化相关
+* [prettier 配置文件 解决自动添加逗号的问题（某马教程的来）](https://blog.csdn.net/m0_58559967/article/details/121294857)
+* [启用 ESLint 后 解决格式化文档自动添加分号和双引号问题](https://developer.aliyun.com/article/1072976)
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,87 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div id="app" class="app">
+    <div class="app__left">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+      >
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+          </template>
+          <el-menu-item-group>
+            <template slot="title">分组一</template>
+            <el-menu-item index="1-1">选项1</el-menu-item>
+            <el-menu-item index="1-2">选项2</el-menu-item>
+          </el-menu-item-group>
+          <el-menu-item-group title="分组2">
+            <el-menu-item index="1-3">选项3</el-menu-item>
+          </el-menu-item-group>
+          <el-submenu index="1-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="1-4-1">选项1</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="2">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="3" disabled>
+          <i class="el-icon-document"></i>
+          <span slot="title">导航三</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
+    </div>
+    <div class="app__right">
+      <router-view/>
+    </div>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
     }
+  }
+}
+</script>
+<style lang="scss">
+html, body {
+  height: 100%;
+  margin: 0;
+}
+</style>
+
+<style lang="scss" scoped>
+.app {
+  height: 100%;
+  display: flex;
+  &__left {
+    height: 100%;
+    width: 210px;
+    .el-menu {
+      height: 100%;
+    }
+  }
+  &__right {
+    flex: 1;
+    min-height: 100%;
+    background: #f0f2f5;
+    padding: 32px;
   }
 }
 </style>
